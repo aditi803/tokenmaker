320f6269bc447126d1d336f382e00385752caa1f
Chandar <chandar@codebrewinnovations.com>
2023-01-09 19:20:34 +0530

    generator page ui changes and skelton

--- src/skeleton/MainSkeleton.jsx
+++ src/skeleton/MainSkeleton.jsx
@@ -14,44 +14,42 @@ const MainSkeleton = () => {
         },
         {
             id: 4
+        },
+        {
+            id: 5
         }
     ]
     return (
-        <div className="chain-select">
-            <>
+        <>
+            <div className="container py-5 my-5">
+                <div className='row'>
+                    {data.map((value, index) => {
+                        // if (index === 0) {
+                        //   return <div className="chain-item">
+                        //     {console.log(value.hrefPath, "jjjjjjj")}
+                        //     <Link to={`/generator/solana`} className="chain-link chain-bsc">
+                        //       <span className="title">{value.categoryName}</span>
+                        //       <span className="logo" style={{ backgroundImage: `url(${imageBaseUrl}${value?.networks[0].networkImage})` }}></span>
+                        //       {/* {value?.networks[0]((img) => (
+                        //         <span className="logo" style={{ backgroundImage: `url(${imageBaseUrl}${img.networkImage})` }}></span>
+                        //       ))} */}
+                        //       <span className="text-muted description">Create your token on {value.categoryName}</span>
+                        //     </Link>
+                        //   </div>
+                        // }else {
+                        return <div className="col-sm-6 col-md-4 col-lg-3 col-xxl">
+                            <div className="chain-item d-flex align-items-center justify-content-center flex-column">
+                                <Skeleton variant="rounded" height={20} width={155} />
 
-                {data.map((value, index) => {
-                    // if (index === 0) {
-                    //   return <div className="chain-item">
-                    //     {console.log(value.hrefPath, "jjjjjjj")}
-                    //     <Link to={`/generator/solana`} className="chain-link chain-bsc">
-                    //       <span className="title">{value.categoryName}</span>
-                    //       <span className="logo" style={{ backgroundImage: `url(${imageBaseUrl}${value?.networks[0].networkImage})` }}></span>
-                    //       {/* {value?.networks[0]((img) => (
-                    //         <span className="logo" style={{ backgroundImage: `url(${imageBaseUrl}${img.networkImage})` }}></span>
-                    //       ))} */}
-                    //       <span className="text-muted description">Create your token on {value.categoryName}</span>
-                    //     </Link>
-                    //   </div>
-                    // }else {
-                    return <div className="chain-item ">
-                        <span className="title d-flex justify-content-center ">
-                            <Skeleton variant="rounded" height={41} width={155} style={{marginTop:"19px"}}/>
-
-                        </span>
-                        <span className="logo text-center d-flex justify-content-center" style={{flexDirection:"column", alignItems:"center"}}>
-                            <Skeleton variant="circular" height={71} width={76} style={{margin:"60px 0 30px"}}/>
-                            <Skeleton variant="rounded" height={62} width={230} />
-                            
-                        </span>
-                        <span className="text-muted description">
-
-                        </span>
-                    </div>
-                    // }
-                })}
-            </>
-        </div>
+                                <Skeleton variant="circular" height={71} width={76} style={{ margin: "60px 0 30px" }} />
+                                <Skeleton variant="rounded" height={62} width={230} />
+                            </div>
+                        </div>
+                        // }
+                    })}
+                </div>
+            </div>
+        </>
     )
 }
 
