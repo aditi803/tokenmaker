58e70627ef5c2f2cf8279db621724334896e2d5c
aditi803 <aditimehta300@gmail.com>
2023-01-10 19:19:23 +0530

    .

--- src/components/pages/Eth_page/EthMain1.jsx
+++ src/components/pages/Eth_page/EthMain1.jsx
@@ -747,12 +747,7 @@ const EthMain1 = (props) => {
         }
 
       // console.log(FormData, "formdata eth side");
-<<<<<<< HEAD
       // if (FormData.network === chainId) {
-=======
-      
-      if (FormData.network === chainId) {
->>>>>>> cfc979e4828aad6e01bd930db044170f8149c58e
         // navigate("/generator/final");
         if (connectedAccAddress.length === 0) {
           await SignInMetamask()
@@ -811,16 +806,9 @@ const EthMain1 = (props) => {
         }
 
 
-<<<<<<< HEAD
       // } else {
       //   changeNetwork(FormData.network);
       // }
-=======
-      } else {
-        changeNetwork(FormData.network);
-        
-      }
->>>>>>> cfc979e4828aad6e01bd930db044170f8149c58e
     } catch (error) {
       toast.error(error.message);
       // console.log("compile contract side catch er", error);
