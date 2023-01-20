3646c711ba0ceeb5bb08494f84db64501ee3c5f5
vicky <vickyhasija167@gmail.com>
2023-01-20 17:29:07 +0530

    taking first pull

--- src/components/pages/landing_page/Header.jsx
+++ src/components/pages/landing_page/Header.jsx
@@ -65,7 +65,8 @@ window.addEventListener('scroll', toggleVisible);
                                         <img className='img-1' aria-hidden='true' src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27257%27%20height=%2725%27/%3e"
                                          alt="" />
                                     </span> */}
-                                        <img src={imageBaseUrl + header.investorLogoImage} alt="Logo" className='logoImage' srcSet="" onClick={scrollToTop} />
+                                        {/* <img src={imageBaseUrl + header.investorLogoImage} alt="Logo" className='logoImage' srcSet="" onClick={scrollToTop} /> */}
+                                        <img src="https://cdn-hnjof.nitrocdn.com/rbqEPUqwZnAoyiJEQENsAgBiOCFoQUNg/assets/images/optimized/rev-1b9374e/wp-content/themes/blockbrew-child/media/2023/01/f9c842d749181b725b2174009ffbe8f4.final_Logo.svg" alt="Logo" className='logoImage' srcSet="" onClick={scrollToTop} />
                                         {/* <img srcSet="https://tokenmaker.eattheblocks.com/_next/image?url=%2Fimages%2Flogo-token-maker.png&w=384&q=75 1x,https://tokenmaker.eattheblocks.com/_next/image?url=%2Fimages%2Flogo-token-maker.png&w=384&q=75 2x"
                                     src='https://tokenmaker.eattheblocks.com/_next/image?url=%2Fimages%2Flogo-token-maker.png&w=384&q=75' decoding='async' data-nimg='intrinsic' 
                                     alt="Logo" className="img-2" /> */}
