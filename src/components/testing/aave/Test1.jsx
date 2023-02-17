29206d101a5e981e2b83d18e2f39f3997e31141f
vicky <vickyhasija167@gmail.com>
2023-02-17 19:13:05 +0530

    regular chng

--- src/components/testing/aave/Test1.jsx
+++ src/components/testing/aave/Test1.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-const { Aave } = require('@aave/protocol-js');
-const { EthereumProvider } = require('@aave/protocol-js/dist/providers');
-const provider = new EthereumProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+// import React from 'react'
+// const { Aave } = require('@aave/protocol-js');
+// const { EthereumProvider } = require('@aave/protocol-js/dist/providers');
+// const provider = new EthereumProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
 
-const aave = new Aave(provider);
-const userAddress = '0x123abc';
-
-
-export const Test1 = async() => {
-  const result = await aave.getUserAccountData(userAddress, 'ETH');
-  console.log(result);
-  return (
-    <>
-    <div>test1
+// const aave = new Aave(provider);
+// const userAddress = '0x123abc';
 
 
-</div>
-    </>
+// export const Test1 = async() => {
+//   const result = await aave.getUserAccountData(userAddress, 'ETH');
+//   console.log(result);
+//   return (
+//     <>
+//     <div>test1
+
+
+// </div>
+//     </>
 
 
     
-  )
-  }
-// export default Test1
+//   )
+//   }
+// // export default Test1
 
 
 
 
 
-// export async function getUserAccountData() {
+// // export async function getUserAccountData() {
   
-// }
+// // }
 
-// getUserAccountData();
+// // getUserAccountData();
